6f370e0dc70c62c96e9086e4b1bafc57c255de83
Juan Pablo <jpvhdeveloper22@gmail.com>
2025-07-11 10:17:18 -0500

    Ajuste del footer en pantallas grandes

--- index.php
+++ index.php
@@ -22,36 +22,36 @@ require './registrar_agotado.php'
             <h2>Surtiviveres la sabana SAS</h2>
         </div>
     </header>
-    <section class="form-section">
+    <main class="form-section">
         <div class="form-container">
-        <form class="form-grid" action="registrar_agotado.php" method="post">
-            <div class="form-group span-1">
-                <label>ID Producto:</label><br>
-                <input type="text" name="id_prod" required>
-            </div>
+            <form class="form-grid" action="registrar_agotado.php" method="post">
+                <div class="form-group span-1">
+                    <label>ID Producto:</label><br>
+                    <input type="text" name="id_prod" required>
+                </div>
 
-            <div class="form-group span-3">
-                <label>Descripción:</label><br>
-                <input type="text" name="descripcion" required>
-            </div>
+                <div class="form-group span-3">
+                    <label>Descripción:</label><br>
+                    <input type="text" name="descripcion" required>
+                </div>
 
-            <div class="form-group span-2">
-                <label>Cantidad en inventario:</label><br>
-                <input type="text" step="0.01" name="cant_inv">
-            </div>
+                <div class="form-group span-2">
+                    <label>Cantidad en inventario:</label><br>
+                    <input type="text" step="0.01" name="cant_inv">
+                </div>
 
-            <div class="form-group span-2">
-                <label>Cantidad sugerida:</label><br>
-                <input type="text" step="0.01" name="cant_sug">
-            </div>
+                <div class="form-group span-2">
+                    <label>Cantidad sugerida:</label><br>
+                    <input type="text" step="0.01" name="cant_sug">
+                </div>
 
-            <div class="button-container span-4">
-                <button class="btn-guardar" type="submit">GUARDAR</button>
-                <button class="btn-limpiar" type="reset">LIMPIAR</button>
-            </div>
-        </form>
+                <div class="button-container span-4">
+                    <button class="btn-guardar" type="submit">GUARDAR</button>
+                    <button class="btn-limpiar" type="reset">LIMPIAR</button>
+                </div>
+            </form>
         </div>
-    </section>
+    </main>
     <footer>
         <p>Aplicativo desarrollado por: Juan Pablo Vargas Hernández - Todos los derechos reservados</p>
         <p>Contacto: 318.625.9804 - Solo WhatsApp</p>
